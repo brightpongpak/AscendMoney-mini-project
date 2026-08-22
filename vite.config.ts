e7ebf2755bb63545b3943022ac84e287f9d0689f
Bright Pongpak <prc32875@gmail.com>
2026-08-23 03:59:00 +0700

    Add CI/CD deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_ACTIONS ? '/AscendMoney-mini-project/' : '/',
   test: {
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
